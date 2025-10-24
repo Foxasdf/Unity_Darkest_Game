a8e94e28ff20b391d02910526a1709f909fd05bf
Your Name <you@example.com>
2025-10-24 16:12:25 +0300

    added Camera Shake

---
+++ Assets/Scripts/ExplosionAnimationEvents.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ExplosionAnimationEvents : MonoBehaviour
+{
+	public void ShakeCamera()
+	{
+		// Make sure CameraShakeTrigger.Instance exists before calling
+		if (CameraShakeTrigger.Instance != null)
+		{
+			CameraShakeTrigger.Instance.ShakeCamera(2f); // You can adjust intensity here
+		}
+		else
+		{
+			Debug.LogWarning("No CameraShakeTrigger found in scene!");
+		}
+	}
+}
