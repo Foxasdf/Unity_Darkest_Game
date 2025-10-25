1f6c98b7645c1ef7e5c8e2c46e498325b9e7262a
Your Name <you@example.com>
2025-10-25 18:39:55 +0300

    added Dialogue System

---
+++ Assets/Scripts/NPC1.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Collections;
+public class NPC1 : MonoBehaviour,IInteractable
+{
+	public NPCDialogue dialogueData;
+	public GameObject dialoguePanel;
+	public TMP_Text dialogueText, nameText;
+	public Image  portraitImage;
+	private int dialogueIndex;
+	private bool isTyping,isDialogueActive;
+
+
+	public bool CanInteract(){
+		return !isDialogueActive;
+	}
+	
+	public void Interact(){
+		if(dialogueData ==null || (PauseController.isGamePaused &&!isDialogueActive)){
+			return;
+		}
+		if (isDialogueActive){
+			NextLine();
+		}
+		else{
+			//Start Dialogue
+			StartDialogue();
+		}
+		
+	}
+	
+	void NextLine(){
+		if(isTyping){
+			StopAllCoroutines();
+			dialogueText.SetText(dialogueData.dialogueLines[dialogueIndex]);
+			isTyping=false;
+		}
+		else if(++dialogueIndex <dialogueData.dialogueLines.Length){
+			StartCoroutine(TypeLine());
+		}
+		else{
+			//end dialogue
+			EndDialogue();
+		}
+	}
+	
+	void StartDialogue(){
+		isDialogueActive=true;
+		dialogueIndex=0;
+		nameText.SetText(dialogueData.npcName);
+		portraitImage.sprite=dialogueData.npcPotrait;
+		
+		dialoguePanel.SetActive(true);
+		PauseController.setPaused(true);
+		
+		//TypeLine
+		StartCoroutine (TypeLine());
+		
+		
+	}
+	IEnumerator TypeLine(){
+		isTyping =true;
+		
+		dialogueText.SetText("");
+		
+		foreach(char letter in dialogueData.dialogueLines[dialogueIndex]){
+			dialogueText.text +=letter;
+			yield return new WaitForSeconds(dialogueData.typingSpeed);
+			
+			
+		}
+		isTyping=false;
+		
+		if(dialogueData.autoProgressLines.Length>dialogueIndex && dialogueData.autoProgressLines[dialogueIndex]){
+			yield return  new WaitForSeconds(dialogueData.autoProgressDelay);
+			NextLine();
+		}
+		
+	}
+	public void EndDialogue(){
+		StopAllCoroutines();
+		isDialogueActive=false;
+		dialogueText.SetText("");
+		dialoguePanel.SetActive(false);
+		PauseController.setPaused(false);
+		
+		
+	}
+
+}
---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PauseController : MonoBehaviour
+{
+	public static bool isGamePaused {get; private set;} =false;
+	public static void setPaused(bool pause){
+		isGamePaused=pause;
+	}
+	
+}
