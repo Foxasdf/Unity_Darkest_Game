3144044194d0b934e7638c19d77c9c9ebcb9e200
Your Name <you@example.com>
2025-11-08 17:11:17 +0300

    added foot steps

---
+++ Assets/Scripts/FootstepSounds.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class FootstepSounds : MonoBehaviour
+{
+	[SerializeField] private AudioSource audioSource;
+	[SerializeField] private AudioClip[] footstepSounds; // Drag your step sounds here
+    
+	private int currentStepIndex = 0;
+
+	// This function is called by Animation Events
+	public void PlayFootstep()
+	{
+		if (footstepSounds.Length == 0) return;
+        
+		// Play a random footstep sound
+		AudioClip clip = footstepSounds[Random.Range(0, footstepSounds.Length)];
+		audioSource.PlayOneShot(clip);
+	}
+    
+	// Or play them in sequence
+	public void PlayFootstepSequential()
+	{
+		if (footstepSounds.Length == 0) return;
+        
+		audioSource.PlayOneShot(footstepSounds[currentStepIndex]);
+		currentStepIndex = (currentStepIndex + 1) % footstepSounds.Length;
+	}
+}
